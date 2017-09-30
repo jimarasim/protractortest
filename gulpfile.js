396406daa62891b1809bed2227163f697b2861b5
JAMES ARASIM <jaemzware@hotmail.com>
2017-09-30 05:07:47 -0700

    fixed outdated files from removed dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,11 @@
 var gulp = require('gulp');
 var angularProtractor = require('gulp-angular-protractor');
 
-
-// Include Our Plugins
-var jshint = require('gulp-jshint');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-
 gulp.task('protractor', function(callback) {
     gulp.src(['./seleniumtests/pinterest*.js'])
         .pipe(angularProtractor({
             'configFile': 'conf.js',
             'debug': false,
-//            'args': ['--baseUrl', 'http://127.0.0.1:8000'],
             'autoStartStopServer': true
         }))
         .on('error', function(e) {
